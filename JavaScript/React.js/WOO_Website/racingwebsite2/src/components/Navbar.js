d8208433624451bd6f143a70711efd151b1bce69
zachrm17 <zmietzner@gmail.com>
2023-10-17 08:49:25 -0500

    Removed button components

--- JavaScript/React.js/WOO_Website/racingwebsite2/src/components/Navbar.js
+++ JavaScript/React.js/WOO_Website/racingwebsite2/src/components/Navbar.js
@@ -1,28 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "./Button";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener("resize", showButton);
 
   return (
     <>
